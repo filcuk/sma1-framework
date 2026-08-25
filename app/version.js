00ad3bf41655bd64f4fd17238e14b8413d90f1ed
filcuk <filcuk@users.noreply.github.com>
2026-08-25 09:15:22 +0100

    Bump release
    Regen manifest

--- app/version.js
+++ app/version.js
@@ -6,7 +6,7 @@
  */
 
 /** @type {`${number}.${number}.${number}`} */
-export const FRAMEWORK_VERSION = "0.12.3";
+export const FRAMEWORK_VERSION = "0.13.0";
 
 /** @type {`${number}.${number}.${number}`} */
 export const APP_VERSION = "0.0.0";
