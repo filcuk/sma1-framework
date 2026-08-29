8317d72dab1cc52b9840e6be189513fa77fd2718
filcuk <filcuk@users.noreply.github.com>
2026-08-29 11:29:11 +0100

    Bump release

--- app/version.js
+++ app/version.js
@@ -6,7 +6,7 @@
  */
 
 /** @type {`${number}.${number}.${number}`} */
-export const FRAMEWORK_VERSION = "0.13.4";
+export const FRAMEWORK_VERSION = "0.13.5";
 
 /** @type {`${number}.${number}.${number}`} */
 export const APP_VERSION = "0.0.0";
